130796cd57a69296a20bb514ebd0c2aebdd6a0fd
xinran-li <55333897+xinran-li@users.noreply.github.com>
2019-10-12 20:05:20 -0700

    Create cone-of-lines.js

---
+++ cone-of-lines.js
@@ -0,0 +1,21 @@
+# HW5
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  for (var i = 10; i <= 390; i = i + 10) {
+  line(200, 0, i, 400);
+  noFill();
+  }
+}
+
